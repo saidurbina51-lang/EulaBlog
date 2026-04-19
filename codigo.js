e853d11083fc53d6ccf3406c6a5c3b488532f963
saidurbina51-lang <saidurbina51@gmail.com>
2026-04-19 02:35:01 -0600

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,47 @@
+function corroborar() {
+    const valoringresado = parseInt (document.getElementById("input").value);
+    const res = 4 ;
+    if (valoringresado===res){
+        respuesta.innerHTML = "Correcto"
+    } else{
+         respuesta.innerHTML = "Incorrecto era " + res;
+    }
+}
+function corroborar2() {
+    const valoringresado2 = parseInt (document.getElementById("input2").value);
+    const res2 = 1 ;
+    if (valoringresado2===res2){
+        respuesta2.innerHTML = "Correcto"
+    } else{
+         respuesta2.innerHTML = "Incorrecto era " + res2;
+    }
+}
+
+function corroborar3() {
+    const valoringresado3 = parseInt (document.getElementById("input3").value);
+    const res3 = 4 ;
+    if (valoringresado3===res3){
+        respuesta3.innerHTML = "Correcto"
+    } else{
+         respuesta3.innerHTML = "Incorrecto era " + res3;
+    }
+}
+function corroborar4() {
+    const valoringresado4 = parseInt (document.getElementById("input4").value);
+    const res4 = 2 ;
+    if (valoringresado4===res4){
+        respuesta4.innerHTML = "Correcto"
+    } else{
+         respuesta4.innerHTML = "Incorrecto era " + res4;
+    }
+}
+
+function corroborar5() {
+    const valoringresado5 = parseInt (document.getElementById("input5").value);
+    const res5 = 3 ;
+    if (valoringresado5===res5){
+        respuesta5.innerHTML = "Correcto"
+    } else{
+         respuesta5.innerHTML = "Incorrecto era " + res5;
+    }
+}
